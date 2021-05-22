1ca12aa6ec674af6e304beda0d09a7c65818f682
Kim Seohyun <kshjessica@ewhain.net>
2021-05-23 01:20:15 +0900

    add auction model/interface

--- src/interfaces/IAuction.ts
+++ src/interfaces/IAuction.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import { IUser } from "./IUser";
 import { IDream } from "./IDream";
+import { IComment } from "./IComment";
 
 export interface IAuction {
     dream : IDream,
     owner : IUser,
     initialPrice : Number,
     currentPrice : Number,
-    comment : [Comment],
+    comment : [IComment],
     enrollDate : Date,
 	dueDate : Date,
     participant : Number,
--- src/models/Auction.ts
+++ src/models/Auction.ts
@@ -2,26 +2,156 @@ import mongoose from "mongoose";
 import { IAuction } from "../interfaces/IAuction";
 
 const AuctionSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+    dream: {
+        image : {
+            type : String,
+            required : true,
+        },
+      
+        content : {
+            type : String,
+            required : true,
+        },
+      
+        keyword : {
+            type : String,
+            required : true,
+        },
+      
+        dreamer : {
+            name: {
+              type: String,
+              required: true,
+            },
+            email: {
+              type: String,
+              required: true,
+              unique: true,
+            },
+            password: {
+              type: String,
+              required: true,
+            },
+            avatar: {
+              type: String,
+            },
+            date: {
+              type: Date,
+              default: Date.now,
+            },
+        },
+      
+        owner : {
+            name: {
+              type: String,
+              required: true,
+            },
+            email: {
+              type: String,
+              required: true,
+              unique: true,
+            },
+            password: {
+              type: String,
+              required: true,
+            },
+            avatar: {
+              type: String,
+            },
+            date: {
+              type: Date,
+              default: Date.now,
+            },
+        },
+      
+          price : {
+            type : Number,
+            required : true,
+        },
+      
+          date : {
+            type : Date,
+            required : true,
+            default : Date.now,
+        },
+      
+          sold : {
+            type : Boolean,
+            required : true,
+        },
+      
+          auctioning : {
+            type : Boolean,
+            required : true,
+        }
+    },
+    owner: {
+        name: {
+            type: String,
+            required: true,
+        },
+          email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+          password: {
+            type: String,
+            required: true,
+        },
+          avatar: {
+            type: String,
+         },
+          date: {
+            type: Date,
+            default: Date.now,
+         },
+    },
+    initialPrice: {
+        type: Number,
+    },
+    currentPrice: {
+        type: Number,
+    },
+    comment: [{
+        writer: {
+            name: {
+                type: String,
+                required: true,
+              },
+              email: {
+                type: String,
+                required: true,
+                unique: true,
+              },
+              password: {
+                type: String,
+                required: true,
+              },
+              avatar: {
+                type: String,
+              },
+              date: {
+                type: Date,
+                default: Date.now,
+              },
+        },
+        biddingPrice: {
+            type: Number,
+        },
+        content: {
+            type: String,
+        },
+    }],
+    enrollDate: {
+        type: Date,
+    },
+    dueDate: {
+        type:  Date,
+    },
+    participant: {
+        type: Number,
+    },
 });
 
 export default mongoose.model<IAuction & mongoose.Document>("Auction", AuctionSchema);
