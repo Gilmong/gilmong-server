8e71847dfd0063413b7e0b6d467f1ba45b15f934
박정무 <bagjeongmu@bagjeongmuui-MacBookAir.local>
2021-05-23 01:22:49 +0900

    feat : add iterface, models

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,9 +1,14 @@
+import mongoose from "mongoose";
+import { IDream } from "./IDream";
+
 export interface IUser {
   name: string;
   email: string;
   password: string;
-  avatar: string;
   date: Date;
+  boughtDream : [ IDream ];
+  soldDream : [ IDream ];
+  coin : number;
 }
 
 export interface IUserInputDTO {
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,70 @@
 import mongoose from "mongoose";
 import { IUser } from "../interfaces/IUser";
 
+const Dream = {
+  image : {
+    type : String,
+    required : true,
+  },
+
+  content : {
+    type : String,
+    required : true,
+  },
+
+  keyword : {
+    type : String,
+    required : true,
+  },
+
+  dreamer : {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+
+  owner : {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+
+  price : {
+    type : Number,
+    required : true,
+  },
+
+  date : {
+    type : Date,
+    required : true,
+    default : Date.now,
+  },
+
+  solded : {
+    type : Boolean,
+    required : true,
+  },
+
+  auctioning : {
+    type : Boolean,
+    required : true,
+  }
+}
+
 const UserSchema = new mongoose.Schema({
+
   name: {
     type: String,
     required: true,
@@ -15,13 +78,23 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  avatar: {
-    type: String,
-  },
   date: {
     type: Date,
     default: Date.now,
   },
+  boughtDream : [{
+    type : Dream,
+    required : false,
+  }],
+  soldDream : [{
+    type : Dream,
+    required : false,
+  }],
+  coin : {
+    type : Number,
+    required : true,
+  }
+  
 });
 
 export default mongoose.model<IUser & mongoose.Document>("User", UserSchema);
